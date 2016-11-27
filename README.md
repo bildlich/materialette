2ca0284a09aba4b20b9a26c4cf3a6aad26524232
Matthias Gieselmann <mgieselmann@Matthiass-MBP-4.silveradsl>
2016-11-27 14:00:33 +0100

    Add ability to identify pasted color values.

--- README.md
+++ README.md
@@ -13,4 +13,10 @@ A little menu bar helper to easily access the mySugr brand and UI colors. ms-col
 * Install dependencies: `$ npm install`
 * Compile SCSS -> CSS `$ gulp sass` or `$ gulp sass:watch`
 * Start the application with `$ npm start `
-* Build the application with  `$ npm run build`  
+* Build the application with  `$ npm run build`
+
+## How to use
+
+Click on the icon in the menu bar and then on a color to copy its color value to the clipboard.
+
+Paste a color code with CMD+V while the ms-colors window is open and it will tell you the name of the color.
--- scripts/main.js
+++ scripts/main.js
@@ -38,6 +38,9 @@ const colors = {
   ]
 };
 
+// Focus invisible textarea so we can listen for paste event
+focusClipboard();
+
 // Populate color cells
 const container = document.getElementById('container');
 for (let name in colors) {
@@ -140,6 +143,8 @@ document.body.addEventListener('click', e => {
       console.log(err);
     }
   }
+  // Focus invisible textarea so we can listen for paste event
+  focusClipboard();
 });
 
 document.body.addEventListener('mouseleave', e => {
@@ -189,4 +194,98 @@ function luminance(sHexColor, sLight, sDark) {
   const oRGB = hexToRgb(sHexColor);
   const yiq = ((oRGB.r * 299) + (oRGB.g * 587) + (oRGB.b * 114)) / 1000;
   return (yiq >= 128) ? sDark : sLight;
+}
+
+function handlePaste (e) {
+    var clipboardData, pastedData, hexColorToInterpret;
+    var colorFound = false;
+
+    // Stop data actually being pasted into div
+    e.stopPropagation();
+    e.preventDefault();
+
+    // Get pasted data via clipboard API
+    clipboardData = e.clipboardData || window.clipboardData;
+    pastedData = clipboardData.getData('Text');
+
+    // Is it a hex value pasted from Sketch, Photoshop or similar? (RRGGBB, #RRGGBB)
+    if (hexToRgb(pastedData)) {
+      console.log('hex value')
+      hexColorToInterpret = pastedData;
+      //alert(hexColorToInterpret);
+    }
+
+    // Is it a value pasted from OSX's "Digital Color Meter" tool? (RR[tab]GG[tab]BB[tab])
+    else if (match = /(\d{1,3})\s+(\d{1,3})\s+(\d{1,3})/.exec(pastedData)) {
+      hexColorToInterpret = rgbToHex(match[1], match[2], match[3]);
+    }
+
+    // If one of the above 2 checks was successful, it looks like a color was pasted
+    if (hexColorToInterpret) {
+      // Normalize: Make letters uppercase
+      hexColorToInterpret = hexColorToInterpret.toUpperCase();
+      // Normalize: Add hash "#" if missing
+      if (hexColorToInterpret.substr(0,1) !== "#") {
+        hexColorToInterpret = "#" + hexColorToInterpret;
+      }
+    }
+    // If no color was found in the clipboard
+    else {
+      showCurtainAnalysisNoColor();
+      return;
+    }
+    // Check if color is in our defined colors
+    for(var mycolors in colors) {
+      for (var color in colors[mycolors]) {
+        if (colors[mycolors][color][1].toUpperCase() === hexColorToInterpret) {
+          colorFound = colors[mycolors][color][0];
+          break;
+        }
+      }
+      if (colorFound != false) { break; }
+    }
+    if (colorFound != false) {
+      showCurtainAnalysisSuccess(colorFound, hexColorToInterpret);
+    }
+    else {
+      showCurtainAnalysisFailure(hexColorToInterpret);
+    }
+}
+
+function showCurtainAnalysisSuccess(colorName, colorHex) {
+  var curtain = document.getElementById('curtain-clipboard-analysis');
+  curtain.innerHTML = "<div class='col-intro'>" + colorHex + "</div>\
+    <div class='cell' style='background-color: " + colorHex + "'></div>\
+    <div class='col-outro'>👍 " + colorName + "</div>";
+  curtain.className = "";
+  setTimeout(function() {
+    curtain.className = "hidden";
+  }, 1400);
+}
+
+function showCurtainAnalysisFailure(colorHex) {
+  var curtain = document.getElementById('curtain-clipboard-analysis');
+  curtain.innerHTML = "<div class='col-intro'>" + colorHex + "</div>\
+    <div class='cell' style='background-color: " + colorHex + "'></div>\
+    <div class='col-outro'>🚫 " + colorHex + " ist nicht in der Sammlung</div>";
+    curtain.className = "";
+    setTimeout(function() {
+      curtain.className = "hidden";
+    }, 1400);
+}
+
+function showCurtainAnalysisNoColor() {
+  var curtain = document.getElementById('curtain-clipboard-analysis');
+  curtain.innerHTML = "<div class='col-intro'>Sorry, das ist kein Farbwert.</div>";
+  curtain.className = "";
+  setTimeout(function() {
+    curtain.className = "hidden";
+  }, 1400); 
+}
+
+document.getElementById('clipboard').addEventListener('paste', handlePaste);
+
+function focusClipboard() {
+  document.getElementById("clipboard").focus();
+  console.log('Clipboard has focus');
 }
